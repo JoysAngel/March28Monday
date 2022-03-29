72898a520f8af3939b907d46f8b6f4f03ada72f7
Joys Angel M <joysangelm@gmail.com>
2022-03-29 10:26:51 +0530

    Changed version

--- test_Calculator.py
+++ test_Calculator.py
@@ -1,23 +1,27 @@
 import Calculator
+import pytest
 
-def test_add2numbers():
-    x=10
-    y=20
-    res=Calculator.add2numbers(x,y)
-    assert res==x+y
+@pytest.mark.parametrize("a,b,c",[(3,2,5),(5,1,6),(8,2,10),(5,2,9)])
+def test_add2numbers(a,b,c):
 
+    res=Calculator.add2numbers(a,b)
+    assert res==c
+
+@pytest.mark.addsub
 def test_sub2numbers():
     x=10
     y=20
     res=Calculator.sub2numbers(x,y)
     assert res==x-y
 
+@pytest.mark.muldiv
 def test_mul2numbers():
     x=10
     y=20
     res=Calculator.mul2numbers(x,y)
     assert res==x*y
 
+@pytest.mark.muldiv
 def test_div2numbers():
     x=10
     y=20
