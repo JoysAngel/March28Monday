1b521188aaca58dd7113b540a7b3737409897299
Joys Angel M <joysangelm@gmail.com>
2022-03-29 10:41:16 +0530

    changed version

--- test_Calculator.py
+++ test_Calculator.py
@@ -1,29 +1,25 @@
 import Calculator
 import pytest
 
-@pytest.mark.parametrize("a,b,c",[(3,2,5),(5,1,6),(8,2,10),(5,2,9)])
+@pytest.mark.parametrize("a,b,c",[(3,2,5),(5,1,6),(8,2,10),(4,3,2,)])
 def test_add2numbers(a,b,c):
 
     res=Calculator.add2numbers(a,b)
     assert res==c
 
-@pytest.mark.addsub
-def test_sub2numbers():
-    x=10
-    y=20
-    res=Calculator.sub2numbers(x,y)
-    assert res==x-y
+@pytest.mark.parametrize("a,b,c",[(9,2,7),(6,1,5),(3,2,1),(5,8,3)])
+def test_sub2numbers(a,b,c):
+    res=Calculator.sub2numbers(a,b)
+    assert res==c
 
-@pytest.mark.muldiv
-def test_mul2numbers():
-    x=10
-    y=20
-    res=Calculator.mul2numbers(x,y)
-    assert res==x*y
+@pytest.mark.parametrize("a,b,c",[(3,2,6),(5,1,5),(8,2,16),(5,2,9)])
+def test_mul2numbers(a,b,c):
 
-@pytest.mark.muldiv
-def test_div2numbers():
-    x=10
-    y=20
-    res=Calculator.div2numbers(x,y)
-    assert res==x/y
+    res=Calculator.mul2numbers(a,b)
+    assert res==c
+
+@pytest.mark.parametrize("a,b,c",[(10,2,5),(4,4,1),(8,2,4),(5,2,9)])
+def test_div2numbers(a,b,c):
+
+    res=Calculator.div2numbers(a,b)
+    assert res==c
